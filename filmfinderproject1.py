f41775df9af9ebddabee23574b2b9d29c7656453
Farbod <96631589+Farbod81@users.noreply.github.com>
2022-01-26 22:43:25 +0330

    Update filmfinderproject1.py

--- filmfinderproject1.py
+++ filmfinderproject1.py
@@ -7,7 +7,7 @@ import json
 import os
 
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-# logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 
